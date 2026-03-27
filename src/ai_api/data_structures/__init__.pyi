0ea823f61806fa2617029d277ae54c7956ad3f3f
Simon Watson <simon.watson.sjw@gmail.com>
2026-03-27 17:41:51 +1100

    [feature] export of all functions in directory.

---
+++ src/ai_api/data_structures/__init__.pyi
@@ -0,0 +1,2 @@
+from .grok import GrokBatchRequest as GrokBatchRequest, GrokBatchResponse as GrokBatchResponse, GrokInput as GrokInput, GrokMessage as GrokMessage, GrokRequest as GrokRequest, GrokResponse as GrokResponse
+from .ollama import OllamaInput as OllamaInput, OllamaMessage as OllamaMessage, OllamaRequest as OllamaRequest, OllamaResponse as OllamaResponse
