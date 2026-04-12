c46dc10296dc8d0f139fe6035d4aefff88571e42
Simon Watson <simon.watson.sjw@gmail.com>
2026-04-12 11:50:11 +1000

    [project] spellings for jinx updated.

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,6 @@
-((nil (pyvenv-activate . "/home/simon/anaconda3/envs/grok/")
-      (ispell-personal-dictionary
-       . "/mnt/HDD04_WDD_08TB/workspace/python/ai_api/.aspell.en.pws")))
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((jinx-dir-local-words . "Postgres infopypg postgres xAI xAIPersistenceManager xai")
+         (pyvenv-activate . "/home/simon/anaconda3/envs/grok/")
+         (ispell-personal-dictionary . "/mnt/HDD04_WDD_08TB/workspace/python/ai_api/.aspell.en.pws"))))
